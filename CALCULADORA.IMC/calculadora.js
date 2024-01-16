f85da86fb76f1f4ed168036b8d251d6653816b2d
AlejandraNegrin <156711809+AlejandraNegrin@users.noreply.github.com>
2024-01-16 20:44:51 +0100

    calculadora

--- CALCULADORA.IMC/calculadora.js
+++ CALCULADORA.IMC/calculadora.js
@@ -2,10 +2,24 @@ document.querySelector("#boton-calcular").addEventListener("click", function(){
     const altura = document.querySelector("#altura").value;
     const peso = document.querySelector("#peso").value;  
     const imc = calcularIMC(peso, altura);
-    document.querySelector("#resultado").innerHTML=imc.toFixed(0);
+    let evaluacion;
+
+    if (imc >= 18.5 && resultado <= 24.9){
+       evalucion="normal";
+    } else if (imc >= 25.0 && resultado <= 29.9){
+        evalucion="sobrepeso";
+    } else if (imc >= 30.0){
+        evaluacion="obesidad";
+    } else {
+        evaluacion="bajo peso";
+    }
+    
+    document.querySelector("#resultado").innerHTML=`${imc.toFixed(1)} (${evaluacion})`;
+
 });
 
 function calcularIMC(peso, altura){
     const imc = peso / (altura**2);
     return imc; 
 }
+
